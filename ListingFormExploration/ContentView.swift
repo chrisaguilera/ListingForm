c7ae07b6509ca5beb657238b3992ee545d570f54
Christopher Aguilera <aguilerachris95@gmail.com>
2022-07-08 09:58:42 -0700

    Initial commit

---
+++ ListingFormExploration/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListingFormExploration
+//
+//  Created by Christopher Aguilera on 7/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ListingFormExploration/ListingFormExplorationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListingFormExplorationApp.swift
+//  ListingFormExploration
+//
+//  Created by Christopher Aguilera on 7/8/22.
+//
+
+import SwiftUI
+
+@main
+struct ListingFormExplorationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
